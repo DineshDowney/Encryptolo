80f608cf28544725d9372d4151e8837974ca008d
Dinesh Garg <dinesh.downey@yahoo.com>
2019-02-25 23:21:59 +0530

    Heroku Stuff

--- encrypto/encrypto/settings.py
+++ encrypto/encrypto/settings.py
@@ -9,7 +9,7 @@ TEMPLATE_DIR=os.path.join(BASE_DIR,"templates")
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'SECRET_KEY'
-DEBUG = True
+DEBUG = False
 DATABASES = {'DATABASE_URL'}
 ALLOWED_HOSTS = ['*']
 
